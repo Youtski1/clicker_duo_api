f9002e468b32a310ef50fdc5864a9993ec8bfd49
you <141744051+Youtski1@users.noreply.github.com>
2026-02-21 11:19:12 +0700

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -5,7 +5,6 @@ exports.default = (0, drizzle_kit_1.defineConfig)({
     dialect: 'postgresql',
     schema: './src/database/models',
     out: "./drizzle",
-    dbCredentials: {
-        url: "postgresql+psycopg://postgres:25814@85.198.83.226:5432/clicker_duo_production"
+    dbCredentials: "
     },
 });
